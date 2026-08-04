2835308fb13f28fcf8224242d1ae75b4ee0d7998
kimmegan <191380108+kimmegan@users.noreply.github.com>
2026-08-04 09:33:14 -0400

    Fix import statement for interp1d in app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from scipy.interpolate import interp1da
+from scipy.interpolate import interp1d
 import plotly.graph_objects as go
 import matplotlib
 matplotlib.use("Agg")
